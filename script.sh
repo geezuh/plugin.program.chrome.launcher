4b3659ac82425bf7cd91995b51c37a6bb52ab39f
Garrett Zuhoski <geezuh@gmail.com>
2020-03-15 14:34:58 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-chromium "--user-data-dir=$HOME/.config/chromium/kodi" --app="$@"
+chromium "--user-data-dir=$HOME/.config/chromium/kodi" --app="$@" --user-agent="Mozilla/5.0 (SMART-TV; Linux; Tizen 4.0.0.2) AppleWebkit/605.1.15 (KHTML, like Gecko) SamsungBrowser/9.2 TV Safari/605.1.15"                                                                    
 echo "$@" > $HOME/.webapp.log
